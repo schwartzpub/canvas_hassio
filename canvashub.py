23371dab73ec4bb8f19895bfbf02591f430aabff
jake@schwartzpub.com <jake@schwartzpub.com>
2025-01-08 11:51:17 -0600

    Revert "we'll get there, curse these classes"
    This reverts commit 7c5a33745122455252d777c23424fc2d7af25c5f.

--- canvashub.py
+++ canvashub.py
@@ -74,7 +74,6 @@ class CanvasHub(DataUpdateCoordinator):
 
         courses = await self.poll_courses()
         for course in courses:
-            _LOGGER.info(course)
             observee = course['enrollments'][0]
             if observee is not None:
                 assignment_tasks.append(asyncio.create_task(self.get_assignments(observee.user_id, course.id, self._semaphore)))
@@ -89,7 +88,6 @@ class CanvasHub(DataUpdateCoordinator):
 
         courses = await self.poll_courses()
         for course in courses:
-            _LOGGER.info(course)
             observee = course['enrollments'][0]
             if observee is not None:
                 submission_tasks.append(asyncio.create_task(self.get_submissions(observee.user_id, course.id, self._semaphore)))
